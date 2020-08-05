ba61a8dedb0e50fcdd67cea5212b172632d4403d
ldz <Merci@mail.dlut.edu.cn>
2020-08-05 18:48:13 +0800

    '2020-08-05-18:48:13'

--- commit_push.sh
+++ commit_push.sh
@@ -2,9 +2,46 @@
 
 username=9p15p
 passwd=ldz19980104
+
 nowtime=$(date "+%Y-%m-%d-%H:%M:%S")
+
+if [ -f ".gitignore" ];then
+	echo ".gtignore存在"
+	if cat '.gitignore' | grep "commit_push.sh" > /dev/null
+		then
+		    echo "commit_push.sh已存在"
+		else
+		    echo "在.gitignore中追加commit_push.sh"
+		    echo "commit_push.sh" >> ./.gitignore
+	fi
+else
+	echo "创建.gitignore中并加入commit_push.sh"
+	cat > .gitignore<<-EOF
+		commit_push.sh	
+	EOF
+fi
 
 git add *
 git commit -m \'$nowtime\'
 
+cat > github.sh <<-EOF
+#!/usr/bin/expect -f
+
+set username [lindex $argv 0]
+set password [lindex $argv 1]
+set nowtime  [lindex $argv 2]
+
+
+spawn git push origin master
+expect "*sername*"
+send "$username\r"
+expect "*assword*"
+send "$password\r"
+interact
+EOF
+
+chmod +x github.sh
+
 expect github.sh $username $passwd $nowtime
+
+rm -rf github.sh
--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/expect -f
-if [ -f "/data/filename" ];then
-echo "文件存在"
-else
-echo "文件不存在"
-fi
+if cat '.gitignore' | grep "commit_push" > /dev/null
+		then
+		    echo "域名为$input_domain已存在"
+		    domain_flag=''
+		    
+	fi
