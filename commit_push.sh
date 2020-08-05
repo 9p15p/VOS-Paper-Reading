aed7c33aa5c8f7c73b880dcd403babe047fb03fb
9p15p <40333030+9p15p@users.noreply.github.com>
2020-08-05 19:09:06 +0800

    Create commit_push.sh
    快捷上传小助手

---
+++ commit_push.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#快捷上传小助手，
+#注意保护好自己的密码
+#需要安装/usr/bin/expect 
+
+username=$github用户名
+password=$github用户名
+
+nowtime=$(date "+%Y-%m-%d-%H:%M:%S")
+
+if [ -f ".gitignore" ];then
+	echo ".gtignore存在"
+	if cat '.gitignore' | grep "commit_push.sh" > /dev/null
+		then
+		    echo "commit_push.sh已存在"
+		else
+		    echo "在.gitignore中追加commit_push.sh"
+		    echo "commit_push.sh" >> ./.gitignore
+	fi
+else
+	echo "创建.gitignore中并加入commit_push.sh"
+	cat > .gitignore<<-EOF
+		commit_push.sh	
+	EOF
+fi
+
+git add *
+git commit -m \'$nowtime\'
+
+cat > github.sh <<-EOF
+#!/usr/bin/expect -f
+
+set username [lindex $argv 0]
+set password [lindex $argv 1]
+
+spawn git push origin master
+expect "*sername*"
+send "$username\n"
+expect "*assword*"
+send "$password\n"
+interact
+EOF
+
+chmod +x github.sh
+
+expect github.sh $username$password
+
+rm -rf github.sh
